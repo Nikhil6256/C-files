cdb61176aceaca039da70f935b2922e50a2ac591
Nikhil <rajc2538@gmail.com>
2024-10-21 01:19:53 +0530

    Add n no. then reverse

--- c/27th.c
+++ c/27th.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    printf("Nope");
+    int n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    int sum = 0;
+    for(int j=n;j>=1;j--){
+        sum += j;
+        printf("%d\n", j);
+    }
+    printf("sum is %d\n",sum);
     return 0;
 }
