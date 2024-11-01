7fedd4d44315ea3cd4bf6dc0615c0cd037e20ef3
Nikhil <rajc2538@gmail.com>
2024-11-02 02:19:53 +0530

    fibonacci series using fun

---
+++ c/52.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+    int fibon(int number);
+int main(){
+    printf("%d", fibon(6));
+    return 0;
+}
+
+    int fibon(int number){
+        if(number == 0) {
+            return 0;
+        }
+        if (number == 1){
+            return 1;
+        }
+        int fibNm1 = fibon(number-1);
+        int fibNm2 = fibon(number-2);
+        int fibN = fibNm1+fibNm2;
+        //printf("fib of %d is : %d\n", number,fibN);
+        return fibN;
+    }
